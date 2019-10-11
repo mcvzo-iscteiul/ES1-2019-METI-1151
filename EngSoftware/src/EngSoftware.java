aedc16b5dc605f466849961eb556aa018a7e1f97
beatriz <bcdds@iscte-iul.pt>
2019-10-11 10:39:55 +0100

    alteracao2

--- EngSoftware/src/EngSoftware.java
+++ EngSoftware/src/EngSoftware.java
@@ -2,7 +2,7 @@
 public class EngSoftware {
 	
 	public static void main(String [] args) {
-		System.out.println("Foi criada a classe. Outra vez");
+		System.out.println("Foi criada a classe. Outra vez.");
 	}
 	
 	
